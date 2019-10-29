68bf27a53e4cca89b81cb33c2f5dd09084b23ae6
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-28 23:26:55 -0500

    Limpiando

--- app/src/main/java/com/nb/animaciones/MainActivity.java
+++ app/src/main/java/com/nb/animaciones/MainActivity.java
@@ -9,27 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView ivCheck;
-    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ivCheck = findViewById(R.id.ivCheck);
-        // Establece animacion creada
-        ivCheck.setBackgroundResource(R.drawable.animation_check);
-
-        //Obtiene animacion
-        animationDrawable = (AnimationDrawable) ivCheck.getBackground();
-
-        ivCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Inicia nuevamente la animacion
-                animationDrawable.start();
-            }
-        });
     }
 }
