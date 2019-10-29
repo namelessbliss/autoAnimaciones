5228f2a3118e1f2f5c169bb2ef26ae36489b4123
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-28 23:49:19 -0500

    Animacion CrossFade sobre textView

--- app/src/main/java/com/nb/animaciones/MainActivity.java
+++ app/src/main/java/com/nb/animaciones/MainActivity.java
@@ -1,19 +1,56 @@
 package com.nb.animaciones;
 
-import android.graphics.drawable.AnimationDrawable;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvContenido;
+    ProgressBar pbLoading;
+    private int duracionAnimacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvContenido = findViewById(R.id.tvContenido);
+        pbLoading = findViewById(R.id.pbLoading);
+
+        // Define tiempo de animacion
+        duracionAnimacion = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+
+        //Ocultar texto
+        tvContenido.setVisibility(View.GONE);
+
+        pbLoading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crossFadeAnimation();
+            }
+        });
+    }
+
+    private void crossFadeAnimation() {
+
+        //Muestra progresivamente el texto
+        tvContenido.setAlpha(0f);
+        tvContenido.setVisibility(View.VISIBLE);
+
+        tvContenido.animate().alpha(1f).setDuration(duracionAnimacion);
+
+        //Oculta progresivamente barra de carga
+        pbLoading.animate().alpha(0f).setDuration(duracionAnimacion).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) { // cuando la animacion finaliza
+                pbLoading.setVisibility(View.GONE);
+            }
+        });
     }
 }
