ff0712ba818c38f0d7592e8b9b8d4b183cfbcb8a
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-29 23:58:57 -0500

    Limpiando

--- app/src/main/java/com/nb/animaciones/MainActivity.java
+++ app/src/main/java/com/nb/animaciones/MainActivity.java
@@ -1,9 +1,6 @@
 package com.nb.animaciones;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,9 +15,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void navegar(View view) {
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this);
-        Intent intent = new Intent(this, SegundoActivity.class);
-        startActivity(intent, options.toBundle());
-    }
 }
